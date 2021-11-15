58126b4b0b0c953002c2c1bbacfe29b9943c3f61
Viktor Besin <e11775831@student.tuwien.ac.at>
2021-11-15 20:53:25 +0100

    fixes foundation check and enumerates rules for foundation

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class ClingoApp(object):
         parse_files(files, lambda stm: term_transformer(stm))
 
         with ProgramBuilder(ctl) as bld:
-            transformer = NglpDlpTransformer(bld, term_transformer.terms)
+            transformer = NglpDlpTransformer(bld, term_transformer.terms, term_transformer.facts, term_transformer.ng_heads)
             parse_files(files, lambda stm: bld.add(transformer(stm)))
             if transformer.counter > 0:
                 parse_string(":- not sat.", lambda stm: bld.add(stm))
@@ -23,18 +23,27 @@ class ClingoApp(object):
                 #parse_string(f"sat :- {','.join([f'sat_r{i}' for i in range(1, transformer.counter+1)])}.", lambda stm: self.bld.add(stm))
                 print(f"sat :- {','.join([f'sat_r{i}' for i in range(1, transformer.counter+1)])}.")
 
+                for p in transformer.f:
+                    for arity in transformer.f[p]:
+                        doms = ','.join(f"dom(D{i})" for i in range(1, arity + 1))
+                        print(f"{{{p}({','.join(f'D{i}' for i in range(1, arity + 1))}) : {doms}}}.")
+                        for c in transformer.f[p][arity]:
+                            # print (c)
+                            # print (','.join([f'r{r}_unfound({c})' for r in transformer.f[p][arity][c]]))
+                            print(f":- {','.join(f'r{r}_unfound({c})' for r in transformer.f[p][arity][c])}.")
+
                 # :- not r1_p_f(X,Z), not r2_p_f(X,Z), ... , rk_p_f(X,Z), p(X,Z).
                 # {p(D0,D1) : dom(D0),dom(D1)}..
-                for p in transformer.foundness:
-                    for arity in transformer.foundness[p]:
-                        if arity > 0:
-                            doms = ','.join(f"dom(D{i})" for i in range (1,arity+1))
-                            vars  = ','.join(f'V{i}' for i in range(1, arity+1))
-                            print(f"{{{p}({','.join(f'D{i}' for i in range (1,arity+1))}) : {doms}}}.")
-                            print(f":- {','.join(f'r{c}_unfound({vars})' for c in transformer.foundness[p][arity])}.")
-                        else:
-                            print(f"{{{p}}}.")
-                            print(f":- {', '.join(f'r{c}_unfound' for c in transformer.foundness[p][arity])}.")
+                # for p in transformer.foundness:
+                #     for arity in transformer.foundness[p]:
+                #         if arity > 0:
+                #             doms = ','.join(f"dom(D{i})" for i in range (1,arity+1))
+                #             vars  = ','.join(f'V{i}' for i in range(1, arity+1))
+                #             print(f"{{{p}({','.join(f'D{i}' for i in range (1,arity+1))}) : {doms}}}.")
+                #             print(f":- {','.join(f'r{c}_unfound({vars})' for c in transformer.foundness[p][arity])}.")
+                #         else:
+                #             print(f"{{{p}}}.")
+                #             print(f":- {', '.join(f'r{c}_unfound' for c in transformer.foundness[p][arity])}.")
                 for t in transformer.terms:
                     print (f"dom({t}).")
 
@@ -44,10 +53,12 @@ class ClingoApp(object):
                             print (f"#show {f}/{l}.")
 
 class NglpDlpTransformer(Transformer):  
-    def __init__(self, bld, terms):
+    def __init__(self, bld, terms, facts, ng_heads):
         self.ng = False        
         self.bld = bld
         self.terms = terms
+        self.facts = facts
+        self.ng_heads = ng_heads
 
         self.cur_anon = 0
         self.cur_var = []
@@ -55,7 +66,9 @@ class NglpDlpTransformer(Transformer):
         self.cur_func_sign = []
         self.shows = {}
         self.foundness ={}
+        self.f = {}
         self.counter = 0
+        self.g_counter = 'A'
 
     def _reset_after_rule(self):
         self.cur_var = []
@@ -119,37 +132,81 @@ class NglpDlpTransformer(Transformer):
             if head is not None:
                 var = re.sub(r'^.*?\(', '', str(head))[:-1].split(',')
                 rem = [v for v in self.cur_var if v not in var] # remaining variables not included in head atom
+                # head_pred = str(head).split("(", 1)[0] # head predicate without arguments
 
-                for r in rem:
-                    # 1{r1_Z(D,X,Y) : dom(D)}1 :- p(X,Y).
-                    print (f"1{{r{self.counter}_{r}({','.join([r]+var)}) : dom({r})}}1 :- {head}.")
+                combinations = [p for p in itertools.product(self.terms, repeat=len(var)+len(rem))]
+                for c in combinations:
+                    interpretation_head = ','.join(c[:-len(rem)] if len(rem) > 0 else c)
+                    if head.name in self.facts and len(var) in self.facts[head.name] and interpretation_head in self.facts[head.name][len(var)]:
+                        # no foundation check for this combination, its a fact!
+                        continue
+                    interpretation = []
+                    for v in var:
+                        interpretation.append(c[var.index(v)])
+                    head_interpretation = head.name + f'({",".join(interpretation)})' if len(var) > 0 else head
+                    for r in rem:
+                        # 1{r1_Z(D,X,Y) : dom(D)}1 :- p(X,Y).
+                        print(f"1{{r{self.counter}_{r}({','.join([c[len(var)+rem.index(r)]] + interpretation)})}}1 :- {head_interpretation}.")
 
-                for f in self.cur_func:
-                    if f != head:
-                        # TODO: check if head has arguments
-                        f_var = re.sub(r'^.*?\(', '', str(f))[:-1].split(',')
-                        f_rem = [f"r{self.counter}_{v}({','.join([v]+var)})" for v in f_var if v in rem]
-                        # r1_unfound(V1,V2) :- p(V1,V2), not f(Z), r1_Z(Z,V1,V2).
-                        print (f"r{self.counter}_unfound({','.join(var)}) :- "
-                               f"{', '.join([str(head)]+[f'not {str(f)}' if not self.cur_func_sign[self.cur_func.index(f)] else str(f)] + f_rem)}.")
+                    for f in self.cur_func:
+                        if f != head:
+                            # TODO: check if head has arguments
+                            f_var = re.sub(r'^.*?\(', '', str(f))[:-1].split(',')
+                            f_rem = [f"r{self.counter}_{v}({','.join([c[len(var)+rem.index(v)]] + interpretation)})" for v in f_var if v in rem]
+                            atom = ""
+                            for v in f_var:
+                                atom += f"{c[self.cur_var.index(v)]}," if v in var else f"{c[len(var)+rem.index(v)]},"
+
+                            if len(atom) > 0:
+                                atom = f"{f.name}({atom[:-1]})"
+                            else:
+                                atom = f"{f.name}"
+                            f_interpretation = '' if self.cur_func_sign[self.cur_func.index(f)] else 'not ' + atom
+                            # r1_unfound(V1,V2) :- p(V1,V2), not f(Z), r1_Z(Z,V1,V2).
+                            print(f"r{self.counter}_unfound({','.join(interpretation)}) :- "
+                                  f"{', '.join([head_interpretation] + [f_interpretation] + f_rem)}.")
+
+                    self._addToFoundednessCheck(head.name, len(var), interpretation_head, self.counter)
+
+
+                # for r in rem:
+                #     # 1{r1_Z(D,X,Y) : dom(D)}1 :- p(X,Y).
+                #     print (f"1{{r{self.counter}_{r}({','.join([r]+var)}) : dom({r})}}1 :- {head}.")
+                #
+                # for f in self.cur_func:
+                #     if f != head:
+                #         # TODO: check if head has arguments
+                #         f_var = re.sub(r'^.*?\(', '', str(f))[:-1].split(',')
+                #         f_rem = [f"r{self.counter}_{v}({','.join([v]+var)})" for v in f_var if v in rem]
+                #         # r1_unfound(V1,V2) :- p(V1,V2), not f(Z), r1_Z(Z,V1,V2).
+                #         print (f"r{self.counter}_unfound({','.join(var)}) :- "
+                #                f"{', '.join([str(head)]+[f'not {str(f)}' if not self.cur_func_sign[self.cur_func.index(f)] else str(f)] + f_rem)}.")
 
                 # r1_p_f(X,Z) :- b(X,Y),c(Y,Z), r1_Y_f(Y).
                 # print(f"r{self.counter}_{head.name}_f({','.join(var)}) :- "
                 #              f"{','.join([f'not {str(f)}' if self.cur_func_sign[self.cur_func.index(f)] else str(f) for f in self.cur_func[1:]])}"
                 #              f"{fixed}.")
-
-                # for :- not r1_p_f(X,Z), not r2_p_f(X,Z), ... , rk_p_f(X,Z), p(X,Z).
-                if head.name not in self.foundness:
-                    self.foundness[head.name] = {}
-                    self.foundness[head.name][len(var)] = [self.counter]
-                elif len(var) not in self.foundness[head.name]:
-                    self.foundness[head.name][len(var)] = [self.counter]
-                else:
-                    self.foundness[head.name][len(var)].append(self.counter)
-
             self._reset_after_rule()
 
         else:
+            # foundation needed?
+            pred = str(node.head).split('(', 1)[0]
+            arguments = re.sub(r'^.*?\(', '', str(node.head))[:-1].split(',')
+            arity = len(arguments)
+            arguments = ','.join(arguments)
+
+            if pred in self.ng_heads and arity in self.ng_heads[pred] \
+                    and not (pred in self.facts and arity in self.facts[pred] and arguments in self.facts[pred][arity]):
+
+                for body_atom in node.body:
+                    if str(body_atom).startswith("not "):
+                        neg = ""
+                    else:
+                        neg = "not "
+                    print(f"r{self.g_counter}_unfound({arguments}) :- "
+                          f"{ neg + str(body_atom)}.")
+                self._addToFoundednessCheck(pred, arity, arguments, self.g_counter)
+                self.g_counter = chr(ord(self.g_counter) + 1)
             # print rule as it is
             print(node)
         return node
@@ -182,16 +239,57 @@ class NglpDlpTransformer(Transformer):
 
     def visit_SymbolicTerm(self, node):
         return node
+    
+    def _addToFoundednessCheck(self, pred, arity, combination, rule):
+        if pred not in self.f:
+            self.f[pred] = {}
+            self.f[pred][arity] = {}
+            self.f[pred][arity][combination] = {rule}
+        elif arity not in self.f[pred]:
+            self.f[pred][arity] = {}
+            self.f[pred][arity][combination] = {rule}
+        elif combination not in self.f[pred][arity]:
+            self.f[pred][arity][combination] = {rule}
+        else:
+            self.f[pred][arity][combination].add(rule)
 
 
-# TODO: save facts
 class TermTransformer(Transformer):
     def __init__(self):
         self.terms = []
+        self.facts = {}
+        self.ng_heads = {}
+        self.ng = False
         self.shows = False
 
+    def visit_Rule(self, node):
+        self.visit_children(node)
+        pred = str(node.head).split('(', 1)[0]
+        arguments = re.sub(r'^.*?\(', '', str(node.head))[:-1].split(',')
+        arity = len(arguments)
+        if self.ng and str(node.head) != "#false":
+            # save pred and arity for later use
+            if pred not in self.ng_heads:
+                self.ng_heads[pred] = {arity}
+            else:
+                self.ng_heads[pred].add(arity)
+            self.ng = False
+        elif node.body.__len__() == 0:
+            arguments = ','.join(arguments)
+            if pred not in self.facts:
+                self.facts[pred] = {}
+                self.facts[pred][arity] = {arguments}
+            elif arity not in self.facts[pred]:
+                self.facts[pred][arity] = {arguments}
+            else:
+                self.facts[pred][arity].add(arguments)
+        return node
+
+    def visit_Variable(self, node):
+        self.ng = True
+        return node
+
     def visit_Interval(self, node):
-        print(node)
         for i in range(int(str(node.left)), int(str(node.right))+1):
             if (str(i) not in self.terms):
                 self.terms.append(str(i))
@@ -206,6 +304,7 @@ class TermTransformer(Transformer):
         self.shows = True
         print (node)
         return node
+
 
 if __name__ == "__main__":
     # no output from clingo itself
