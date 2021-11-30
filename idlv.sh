6af76638826497c7d7ec0a5a0c3589b03b205483
Markus Hecher <mhecher@gmail.com>
2021-11-30 16:35:02 +0100

    updated idlc script

--- idlv.sh
+++ idlv.sh
@@ -1,1 +1,2 @@
-../I-DLV/idlv_1.1.6_linux_x86-64 --print-rewriting --output 1 $1 #instances/grounding-issue4.lp
+#../I-DLV/idlv_1.1.6_linux_x86-64 --print-rewriting --output 1 $1 #instances/grounding-issue4.lp
+../I-DLV/idlv_1.1.6_linux_x86-64 --output 1 $1 #instances/grounding-issue4.lp
