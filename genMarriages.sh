88febe78071f3f9cb5685d072d4ad3b9df02d291
Markus Hecher <hecher@cobra-submit.kr.tuwien.ac.at>
2022-01-01 21:24:46 +0100

    added newground+idlv

--- genMarriages.sh
+++ genMarriages.sh
@@ -1,8 +1,12 @@
-for i in `seq 100 100 1500` 
+for i in `seq 100 10 200` 
 do
-	for j in `seq 10 10 100`
+	for j in `seq 5 5 100`
 	do
 		echo "size $i, prob $j"
-		python3 genMarriage.py $i $j > instances/marriage_graphs/random_${i}_${j}.lp
+		if [ -f "instances/marriage_graphs/random_${i}_${j}.lp" ]; then
+			echo "file exists"
+		else
+			python3 genMarriage.py $i $j > instances/marriage_graphs/random_${i}_${j}.lp
+		fi
 	done
 done
---
+++ solve-newground-idlv.sh
@@ -0,0 +1,1 @@
+$(dirname "$0")/newground.sh $* |  $(dirname "$0")/idlv_1.1.6_linux_x86-64 --output 1 /dev/stdin | $(dirname "$0")/vanilla-clingo.sh
